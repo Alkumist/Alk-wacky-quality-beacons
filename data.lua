9c8bb7dde5d8f85b72e1fd885dae229945dd16a6
Alkumist <ponyyouman@gmail.com>
2025-09-29 00:47:03 -0600

    Add files via upload

---
+++ data.lua
@@ -0,0 +1,9 @@
+data.raw.beacon.beacon.profile = {3,0.7071 * 2,0.5773,0,0.4472 * 2,0.4082,0.3779,0.125,0.3333,0.3162,0.3015,0.2886,0,0,0,0,0,0,0.2294 * 1.3,0.2236,0.2182,0.2132,0.2085,0.2041,0.2,0.1961,0.1924,0.1889,0.1856,0.1825,0.1796,0.1767,0.174,0.1714,0.169,0.1666,0.1643,0.1622,0.1601,0.1581,0.1561,0.1543,0.1524,0.1507,0.149,0.1474,0.1458,0.1443,0.1428,0.1414,0.14,0.1386,0.1373,0.136,0.1348,0.1336,0.1324,0.1313,0.1301,0.129,0.128,0.127,0.1259,0.125,0.124,0.123,0.1221,0.1212,0.1203,0.1195,0.1186,0.1178,0.117,0.1162,0.1154,0.1147,0.1139,0.1132,0.1125,0.1118,0.1111,0.1104,0.1097,0.1091,0.1084,0.1078,0.1072,0.1066,0.1059,0.1054,0.1048,0.1042,0.1036,0.1031,0.1025,0.102,0.1015,0.101,0.1005,0.1}
+
+data.raw.beacon.beacon.quality_affects_module_slots = true
+data.raw.beacon.beacon.quality_affects_supply_area_distance = true
+data.raw.beacon.beacon.supply_area_distance = 9
+data.raw.quality.uncommon.beacon_module_slots_bonus = 2
+data.raw.quality.rare.beacon_module_slots_bonus = 4
+data.raw.quality.epic.beacon_module_slots_bonus = 6
+data.raw.quality.legendary.beacon_module_slots_bonus = 8
